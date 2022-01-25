3dca6ff2808dbe656bc7d2df037e364513e6bc74
flavio manzotti <manzottiflavio22@gmail.com>
2022-01-25 16:21:22 -0300

    post carIn and carOut ok

--- src/index.js
+++ src/index.js
@@ -8,42 +8,75 @@ app.listen(8888);
 const user=[];
 
 function veryfyIfUserExists(request,response,next){
-const {cpf}=request.headers;
-
-const users=user.find((users)=>users.cpf===cpf);
-
-if(!users){
-    return response.status(400).json({message:"user not found"});
+    const {cpf}=request.headers;
+    
+    const users=user.find((users)=>users.cpf===cpf);
+    
+    if(!users){
+        return response.status(400).json({message:"user not found"});
     }
-
-request.users=users;
-return next();
+    
+    request.users=users;
+    return next();
 };
 
 
 app.post("/user",(request,response)=>{
     const {name,cpf}=request.body;
-
-const users=user.some((users)=>users.cpf===cpf);
-
-if(users){
-    return response.status(201).json({message:"error users already exists"})
-}
-
-user.push({
-name,
-cpf,
-id:uuidv4(),
-creat_at:new Date(),
-relatorio:[],
-});
-console.log(user);
-return response.status(201).json({message:"user create with sucessfull"});
-
+    
+    const users=user.some((users)=>users.cpf===cpf);
+    
+    if(users){
+        return response.status(201).json({message:"error users already exists"})
+    }
+    
+    user.push({
+        name,
+        cpf,
+        id:uuidv4(),
+        creat_at:new Date(),
+        relatorio:[],
+    });
+    
+    return response.status(201).json({message:"user create with sucessfull"});
+    
 });
 
 app.get("/user",veryfyIfUserExists,(request,response)=>{
     const {users}=request;
-
+    
     return response.status(201).json(users);
+});
+
+
+app.post("/carIn",veryfyIfUserExists,(request,response)=>{
+    const{users}=request;
+    const{carModel,cor}=request.body;
+    
+    
+    const getIn={
+        carModel,
+        cor,
+        enter_at:new Date(),
+    }
+    users.relatorio.push(getIn);
+
+   
+        return response.status(201).json({message:"car enter in garage"})
+   
+   
+    
+});
+
+app.post("/carOut",veryfyIfUserExists,(request,response)=>{
+    const {users}=request;
+    const {carModel,cor}=request.body;
+    
+    const getOut={
+        carModel,
+        cor,
+        enter_at:new Date(),
+    }
+    users.relatorio.push(getOut);
+    return response.status(201).json({message:"car out garage"})
 })
